1efff3ab66cc4c7a1443093115a45f30b32b274b
akuto aoyama <aaaa.akuto@gmail.com>
2020-05-26 01:54:39 +0900

    HomePage.tsx編集

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,29 +1,34 @@
-import * as React from "react";
+import * as React from 'react';
 import { Home } from '../pages/Home';
 import { Profile } from '../pages/Profile';
 import { ZeitSample } from '../pages/zeit-sample';
-// import { Layout } from './layout'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
-import { Header } from './header'
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Header } from './header';
+
+// style: {
+//     height: number;
+//     background: string;
+// }
+
+const styles = {
+  header: {
+    height: 100,
+    background: '#0000000',
+  },
+  main: {
+    height: 200,
+  },
+  footer: {
+    height: 100,
+    background: '#1111111',
+  },
+};
 
 export const HomePage = () => (
   <BrowserRouter>
-      <Header />
-      <Route exact path='/' component={Home} />
-      <Route path='/profile' component={Profile} />
-      <Route path='/zeit-sample' component={ZeitSample} />
+    <Header style={styles.header} />
+    <Route exact path="/" component={Home} />
+    <Route path="/profile" component={Profile} />
+    <Route path="/zeit-sample" component={ZeitSample} />
   </BrowserRouter>
-)
-const styles = {
-    header: {
-        height: 100,
-        background: "#ddd",
-    },
-    main: {
-        height: 200,
-    },
-    footer: {
-        height: 100,
-        background: "#ddd",
-    }
-}
+);
