bffb85188e213c344b1dd92ce8dd6b9c8c692833
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-02 12:56:30 +0900

    src/pages/Posts.tsx修正

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,49 +1,49 @@
-import React, { useState, useEffect } from 'react';
-
-export const Posts = () => {
-  const [hasError, setErrors] = useState(false);
-  const [planets, setPlanets] = useState({});
-
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch('https://swapi.co/api/planets/4/');
-      res
-        .json()
-        .then((res) => setPlanets(res))
-        .catch((err) => setErrors(err));
-    }
-
-    fetchData();
-  });
-
-  return (
-    <div>
-      <span>{JSON.stringify(planets)}</span>
-      <hr />
-      <span>Has error: {JSON.stringify(hasError)}</span>
-    </div>
-  );
-};
-
-// export function Posts(): Response {
-// export const Posts = async () => {
 // export function Posts<T>(): Promise<T> {
-
 //   const url: string = process.env.REACT_APP_API_URL_POST_DATAS!;
-//   return await fetch(url, {
+//   return fetch(url, {
 //     method: 'get',
 //   }).then((response) => {
 //     if (!response.ok) {
 //       throw new Error(response.statusText);
 //     }
-//     // return response.json() as Promise<T>;
-//     return response.json();
+//     return response.json() as Promise<T>;
+//   });
+// }
+
+// import React, { useState, useEffect } from 'react';
+
+// export const Posts = (): void => {
+//   // const [hasError, setErrors] = useState(false);
+
+//   (async function () {
+//     const url: string = process.env.REACT_APP_API_URL_POST_DATAS!;
+
+//     const res = await fetch(url);
+//     await res.json();
+//     console.log(res);
 //   });
 // };
 
-// export const Posts = () => {
-//   const url: string = process.env.REACT_APP_API_URL_POST_DATAS!;
-//   return fetch(url, {
-//     method: 'get',
-//   });
-// };
+import React, { useState, useEffect } from 'react';
+export const Posts = () => {
+  const [hasError, setErrors] = useState(false);
+  const [posts, setPosts] = useState({});
+  useEffect(() => {
+    async function fetchData() {
+      const url: string = process.env.REACT_APP_API_URL_POST_DATAS!;
+      const res = await fetch(url);
+      res
+        .json()
+        .then((res) => setPosts(res))
+        .catch((err) => setErrors(err));
+    }
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <span>{JSON.stringify(posts)}</span>
+      <hr />
+      <span>Has error: {JSON.stringify(hasError)}</span>
+    </div>
+  );
+};
