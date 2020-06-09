aba5ac0611527afda39fb4185cb668734f5e6bb2
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-09 23:49:30 +0900

    [delete]src/pages/Posts.tsxのコメント削除

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,29 +1,3 @@
-// export function Posts<T>(): Promise<T> {
-//   const url: string = process.env.REACT_APP_API_URL_POST_DATAS!;
-//   return fetch(url, {
-//     method: 'get',
-//   }).then((response) => {
-//     if (!response.ok) {
-//       throw new Error(response.statusText);
-//     }
-//     return response.json() as Promise<T>;
-//   });
-// }
-
-// import React, { useState, useEffect } from 'react';
-
-// export const Posts = (): void => {
-//   // const [hasError, setErrors] = useState(false);
-
-//   (async function () {
-//     const url: string = process.env.REACT_APP_API_URL_POST_DATAS!;
-
-//     const res = await fetch(url);
-//     await res.json();
-//     console.log(res);
-//   });
-// };
-
 import React, { useState, useEffect } from 'react';
 import { FetchData } from '../scripts/api/FetchData';
 
