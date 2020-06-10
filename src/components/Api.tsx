b376020e637e2a0ba30ecf7fb32c43d45e58cf8e
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-10 17:28:53 +0900

    [add]src/components/Api.tsxにlogin,logout.registration追加

--- src/components/Api.tsx
+++ src/components/Api.tsx
@@ -1,43 +1,59 @@
+type LoginParams = {
+  email: string;
+  password: string;
+};
 export const sessionApi = {
-  login: ({ email, password }) => {
-    return axios
-      .post('/users/sign_in', {
-        user: {
-          email: email,
-          password: password,
-          remember_me: 1,
-        },
-      })
-      .then((response) => {
-        console.log('success');
-        updateCsrfToken(response.data.csrf_token);
-        return response;
-      });
+  login: ({ email, password }: LoginParams) => {
+    const loginFormData = {
+      user: {
+        email: email,
+        password: password,
+        remember_me: 1,
+      },
+    };
+    fetch('/users/sign_in', {
+      method: 'POST',
+      body: JSON.stringify(loginFormData),
+    }).then((response) => {
+      console.log('success');
+      // updateCsrfToken(response.loginFormData.csrf_token);
+      return response;
+    });
   },
   logout: () => {
-    return axios.delete('/users/sign_out').then((response) => {
+    fetch('/users/sign_out', {
+      method: 'DELETE',
+    }).then((response) => {
       console.log('success');
-      updateCsrfToken(response.data.csrf_token);
+      // updateCsrfToken(response.formData.csrf_token);
       return response;
     });
   },
 };
 
+type RejistrationParams = {
+  email: string;
+  password: string;
+  password_confirmation: string;
+  name: string;
+};
 export const registrationApi = {
-  signUp: ({ email, password, password_confirmation, name }) => {
-    return axios
-      .post('/users', {
-        user: {
-          name: name,
-          email: email,
-          password: password,
-          password_confirmation: password_confirmation,
-        },
-      })
-      .then((response) => {
-        console.log('success');
-        updateCsrfToken(response.data.csrf_token);
-        return response;
-      });
+  signUp: ({ email, password, password_confirmation, name }: RejistrationParams) => {
+    const registrationFormData = {
+      user: {
+        name: name,
+        email: email,
+        password: password,
+        password_confirmation: password_confirmation,
+      },
+    };
+    fetch('/users', {
+      method: 'POST',
+      body: JSON.stringify(registrationFormData),
+    }).then((response) => {
+      console.log('success');
+      // updateCsrfToken(response.registrationFormData.csrf_token);
+      return response;
+    });
   },
 };
