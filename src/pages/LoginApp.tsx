c48aa72be3546b6c7af0970dc465535926aadc54
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-11 02:06:22 +0900

    [add]Logout.tsx追加と修正

--- src/pages/LoginApp.tsx
+++ src/pages/LoginApp.tsx
@@ -3,30 +3,30 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 
 import Login from './Login';
 import Logout from './Logout';
-import List1 from './List1';
-import List2 from './List2';
-import Auth from './Auth';
-import User from './User';
+import { HomePage } from '../components/HomePage';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/logout" component={Logout} />
-          <Auth>
-            <Switch>
-              <Route exact path="/list1" component={List1} />
-              <Route exact path="/list2" component={List2} />
-              <Redirect from="/" to="/List1" />
-            </Switch>
-          </Auth>
-        </Switch>
-      </Router>
-    );
-  }
-}
+// import List1 from './List1';
+// import List2 from './List2';
+import { Auth } from '../components/Auth';
+import User from '../components/User';
+
+export const LoginApp = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/logout" component={Logout} />
+        <Auth>
+          <Switch>
+            <Route exact path="/list1" component={HomePage} />
+            {/* <Route exact path="/list2" component={List2} /> */}
+            <Redirect from="/" to="/List1" />
+          </Switch>
+        </Auth>
+      </Switch>
+    </Router>
+  );
+};
 
 // import * as React from 'react';
 // import { useState, useEffect } from 'react';
---
+++ src/pages/Logout.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+// import { Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import User from './User';
+import { useEffect } from 'react';
+
+export const Logout = () => {
+  // async componentDidMount() {
+  //   await User.logout();
+  // }
+  useEffect(() => {
+    const logout = async () => {
+      await User.logout();
+    };
+    logout();
+  }, []);
+
+  render() {
+    return (
+      <Container className="center">
+        <Row className="justify-content-md-center">
+          <div>
+            <h2>ログアウトしました</h2>
+            <div className="text-center">
+              <Link to="/login">ログイン画面へ</Link>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    );
+  }
+}
