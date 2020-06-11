dbbc12a1474ca7985a0545b08b46d5ed6680321a
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-11 21:27:25 +0900

    [add]sessionApi.tsxにheaders等追加

--- src/components/sessionApi.tsx
+++ src/components/sessionApi.tsx
@@ -7,21 +7,35 @@ type LoginParams = {
 
 export const sessionApi = {
   login: ({ email, password }: LoginParams) => {
-    const loginFormData = {
-      user: {
-        email: email,
-        password: password,
-        remember_me: 1,
-      },
+    // const loginFormData = {
+    //   user: {
+    //     email: email,
+    //     password: password,
+    //     // remember_me: 1,
+    //   },
+    // };
+    const obj = {
+      email: email,
+      password: password,
+    };
+    const method = 'POST';
+    const body = JSON.stringify(obj);
+    const headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     };
     // user_session POST
     // /api/v1/auth/sign_in(.: format)
     // devise_token_auth / sessions#create
     // console.log(loginUrl);
-    fetch('http://localhost:3000/api/v1/auth/sign_in', {
-      method: 'POST',
-      body: JSON.stringify(loginFormData),
-    }).then((response) => {
+
+    fetch(
+      'http://localhost:3000/api/v1/auth/sign_in',
+      { method, headers, body },
+      // fetch(logoutUrl, {
+      // method: 'POST',
+      // body: JSON.stringify(loginFormData),
+    ).then((response) => {
       console.log('success');
 
       return response;
@@ -29,6 +43,7 @@ export const sessionApi = {
   },
   logout: () => {
     fetch('http://localhost:3000/api/v1/auth/sign_out', {
+      // fetch(logoutUrl, {
       method: 'DELETE',
     }).then((response) => {
       console.log('success');
