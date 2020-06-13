85e950e316ce1b54928a55fd29937021c19ba5a3
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-14 00:19:43 +0900

    [fix]sessionApiのURLを環境変数に変更

--- src/components/sessionApi.tsx
+++ src/components/sessionApi.tsx
@@ -7,8 +7,6 @@ type LoginParams = {
   email: string;
   password: string;
 };
-// const loginUrl: string = process.env.REACT_APP_API_URL_SIGN_IN!;
-// const logoutUrl: string = process.env.REACT_APP_API_URL_SIGN_OUT!;
 
 export const sessionApi = {
   login: async ({ email, password }: LoginParams) => {
@@ -21,31 +19,21 @@ export const sessionApi = {
     const credentials = 'include';
     const mode = 'cors';
     const headers = {
-      // Accept: 'application/json',
       'Content-Type': 'application/json',
     };
+    const loginUrl: string = process.env.REACT_APP_API_URL_SIGN_IN!;
 
-    return fetch(
-      'http://localhost:3000/api/v1/auth/sign_in',
-      // { method, headers, body, credentials, mode },
-      { method, headers, body },
-    )
+    return fetch(loginUrl, { method, headers, body })
       .then((response) => {
         if (response.status == 200) {
           User.set('isLoggedIn', 'true');
           console.log('isLoggedIn(sessionApi.tsx):', User.isLoggedIn());
           return <App />;
-          // return 'true';
         } else {
           User.set('isLoggedIn', 'false');
           console.log('isLoggedIn(else後):', User.isLoggedIn());
 
-          // console.log('elseのresponse', JSON.stringify(response.json()));
-          // console.log('response.json()', response.json());
-          // throw new HttpError(response);
           throw new Error();
-          // return response.json();
-          // response.text().then((text) => console.log(text));
         }
       })
       .catch((error) => {
@@ -53,43 +41,19 @@ export const sessionApi = {
         console.log('isLoggedIn(catch後):', User.isLoggedIn());
 
         console.log('errorの内容', JSON.stringify(error.json));
-
-        // return 'false';
       });
   },
 
-  //       });
-  // ).then((response) => {
-  //   if (response.ok) {
-  //     // const loginStatus = 'true';
-
-  //     // return loginStatus;
-  //     // return response.text();
-  //     return true;
-  //   } else {
-  //     return false;
-  //     // return Promise.reject(new Error('エラーです！'));
-  //   }
-  // });
-  // .catch((error) => {
-  //   console.log('error');
-  //   console.error(error);
-  //   const loginStatus = 'false';
-  //   // return error;
-  //   return loginStatus;
-  // });
   logout: async () => {
-    await fetch('http://localhost:3000/api/v1/auth/sign_out', {
-      // fetch(logoutUrl, {
-      method: 'DELETE',
-    }).then((response) => {
+    const logoutUrl: string = process.env.REACT_APP_API_URL_SIGN_OUT!;
+
+    await fetch(logoutUrl, { method: 'DELETE' }).then((response) => {
       console.log('success');
       console.log('reaponse(sessionApi.login)');
       console.log(response);
       const loginStatus = 'false';
-      // return error;
+
       return loginStatus;
-      // return response;
     });
   },
 };
