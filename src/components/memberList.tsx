c21914dad8f127e163b4c774eb996a72c7801405
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-24 12:42:14 +0900

    [fix]memberList.tsxでpropsでメソッド等を受け取る

--- src/components/memberList.tsx
+++ src/components/memberList.tsx
@@ -1,28 +1,14 @@
-require('dotenv').config();
-
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import { Grid, Row, Note, Button, Divider } from '@zeit-ui/react';
-import * as Icon from '@zeit-ui/react-icons';
-import User from '../components/User';
-import { FetchData } from '../scripts/api/FetchData';
-import { List } from '../components/List';
+require("dotenv").config();
+import React from "react";
+import { List } from "../components/List";
 
 export const MemberList = (props: any): any => {
-  const followUsersList: any = props.followUsers.map((e: any) => {
-    return e['id'];
-  });
-
-  // const List = props.fetchUsers.map((user: any) => {
 
   return (
-    // <>
     <div>
       {props.fetchUsers.map((user: any) => (
-        <List user={user} followUsersList={followUsersList} />
-        // <List followUsersList={followUsersList} />
+        <List user={user} followUsersList={props.followUsers} pushToFollowUsers={props.pushToFollowUsers} removeFromFollowUsers={props.removeFromFollowUsers} />
       ))}
     </div>
-    // </>
   );
 };
