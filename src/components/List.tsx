4000349679c10c08b81b32d12c4305062ff48b2a
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-24 17:34:18 +0900

    [fix]List.tsxにdivを追加し、表示上下位置を修正

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -68,36 +68,37 @@ export const List = (props: any) => {
   return (
     <>
       <Row>
-        {/* <ul className="list-none"> */}
-        {/* <ul> */}
-        {/* <li key={props.user.id} className="list-none" style={{ color: 'gray' }}> */}
-        <li key={props.user.id} style={{ color: 'white' }}>
-          <Link to={'/profilepage/' + props.user.id}>{props.user.name}&emsp;</Link>
-          {props.followUsersList.includes(props.user.id) ? (
-            <Button
-              type="warning"
-              size="small"
-              auto
-              ghost
-              onClick={() => onClickUnFollow(props.user.id)}
-            >
-              <Icon.EyeOff size={16} />
-              UnFollow
-            </Button>
-          ) : (
-            <Button
-              type="success"
-              size="small"
-              auto
-              ghost
-              onClick={() => onClickFollow(props.user.id)}
-            >
-              <Icon.Eye size={16} />
-              Follow
-            </Button>
-          )}
-        </li>{' '}
-        {/* </ul> */}
+        <div className="flex items-center">
+          <div className="flex-1  text-center">
+            <li key={props.user.id} style={{ color: 'white' }} className="flex items-center m-auto">
+              <Link to={'/profilepage/' + props.user.id}>{props.user.name}&emsp;</Link>
+              {props.followUsersList.includes(props.user.id) ? (
+                <Button
+                  type="warning"
+                  size="small"
+                  auto
+                  ghost
+                  onClick={() => onClickUnFollow(props.user.id)}
+                  className="m-auto"
+                >
+                  <Icon.EyeOff size={16} />
+                  UnFollow
+                </Button>
+              ) : (
+                <Button
+                  type="success"
+                  size="small"
+                  auto
+                  ghost
+                  onClick={() => onClickFollow(props.user.id)}
+                >
+                  <Icon.Eye size={16} />
+                  Follow
+                </Button>
+              )}
+            </li>{' '}
+          </div>
+        </div>
       </Row>
       <Divider />
     </>
