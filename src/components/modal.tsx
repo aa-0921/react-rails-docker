51afca77092e85b51513ec37e2c6f0cb347e9322
akuto aoyama <aaaa.akuto@gmail.com>
2020-06-25 18:03:42 +0900

    [add]modal.tsx追加し、表示成功

---
+++ src/components/modal.tsx
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+import React, { useState, useEffect } from 'react';
+import { Modal, Button } from '@zeit-ui/react';
+
+export const modal = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const handler = () => setModalOpen(true);
+  const closeHandler = () => {
+    setModalOpen(false);
+    console.log('closed');
+  };
+  return (
+    <div>
+      <Button auto onClick={handler}>
+        Show Modal
+      </Button>
+      <Modal open={modalOpen} onClose={closeHandler}>
+        <Modal.Title>Modal</Modal.Title>
+        <Modal.Subtitle>This is a modal</Modal.Subtitle>
+        <Modal.Content>
+          <p>Some content contained within the modal.</p>
+        </Modal.Content>
+        <Modal.Action passive onClick={() => setModalOpen(false)}>
+          Cancel
+        </Modal.Action>
+        {/* <Modal.Action>Submit</Modal.Action> */}
+      </Modal>
+    </div>
+  );
+};
